7fefa62766622fac8b67f348fed6a51bbdbdb054
Weverton Icaro <weverton.icaro@infosistemas.com.br>
2023-08-24 23:57:59 -0300

    adição rota de login

--- src/http/routes/index.ts
+++ src/http/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
+import { authRouter } from "./loginRoutes";
 import { userRouter } from "./userRoutes";
 
 const router = Router();
 
 router.use('/users', userRouter)
+router.use('/auth', authRouter)
 
 export { router };
