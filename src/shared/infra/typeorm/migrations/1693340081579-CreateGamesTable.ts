67fc1f505d3986481699c57144edf72f5530f7d8
Heuder Rodrigues de Sena <documento_arquivo@hotmail.com>
2023-08-29 18:35:19 -0300

    Criando migration table games

---
+++ src/shared/infra/typeorm/migrations/1693340081579-CreateGamesTable.ts
@@ -0,0 +1,86 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class createGamesTable1693340081579 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await queryRunner.createTable(
+      new Table({
+        name: "games",
+        columns: [
+          {
+            name: "id",
+            type: "int",
+            isPrimary: true,
+            isGenerated: true,
+            generationStrategy: "increment"
+          },
+          {
+            name: "name",
+            type: "varchar"
+          },
+          {
+            name: "gameId",
+            type: "integer",
+          },
+          {
+            name: "gameCode",
+            type: "varchar",
+          },
+          {
+            name: "enabled",
+            type: "tinyint",
+            default: true
+          },
+          {
+            name: "category",
+            type: "varchar",
+          },
+          {
+            name: "urlThumb",
+            type: "longtext",
+          },
+          {
+            name: "urlSquareThumb",
+            type: "longtext",
+          },
+          {
+            name: "urlBackground",
+            type: "longtext",
+          },
+          {
+            name: "providerId",
+            type: "integer",
+            isNullable: false
+          },
+          {
+            name: "createdAt",
+            type: "timestamp",
+            default: "now()"
+          },
+          {
+            name: "updatedAt",
+            type: "timestamp",
+            default: "now()"
+          }
+        ],
+        foreignKeys: [
+          {
+            name: "fk_games_providers",
+            columnNames: ["providerId"],
+            referencedTableName: "providers",
+            referencedColumnNames: ["id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE"
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {
+    const tableExist = await queryRunner.hasTable('games');
+    if (!tableExist) {
+      await queryRunner.dropTable('games');
+    }
+  }
+}
