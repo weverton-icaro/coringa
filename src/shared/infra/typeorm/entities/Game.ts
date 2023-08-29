860a502b27a51c6efb50c82358ae9f90926fcd14
Heuder Rodrigues de Sena <documento_arquivo@hotmail.com>
2023-08-29 20:46:27 -0300

    Criando entidties Game

---
+++ src/shared/infra/typeorm/entities/Game.ts
@@ -0,0 +1,71 @@
+import moment from "moment-timezone";
+
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn
+} from "typeorm";
+import { Provider } from "./Provider";
+
+@Entity('games')
+export class Game {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  gameId: number;
+
+  @Column()
+  gameCode: string;
+
+  @Column()
+  enabled: boolean;
+
+  @Column()
+  category: string;
+
+  @Column()
+  urlThumb: string;
+
+  @Column()
+  urlSquareThumb: string;
+
+  @Column()
+  urlBackground: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @ManyToOne(type => Provider)
+  @JoinColumn({ name: 'ProviderId' })
+  provider: Provider;
+
+  @BeforeInsert()
+  insertCreated() {
+    this.createdAt = new Date(
+      moment().tz("America/Sao_Paulo").format("YYYY-MM-DD HH:mm:ss")
+    );
+    this.updatedAt = new Date(
+      moment().tz("America/Sao_Paulo").format("YYYY-MM-DD HH:mm:ss")
+    );
+  }
+
+  @BeforeUpdate()
+  insertUpdated() {
+    this.updatedAt = new Date(
+      moment().tz("America/Sao_Paulo").format("YYYY-MM-DD HH:mm:ss")
+    );
+  }
+}
